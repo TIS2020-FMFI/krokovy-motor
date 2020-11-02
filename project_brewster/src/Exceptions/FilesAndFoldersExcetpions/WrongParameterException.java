32a62a01bb14bc8e0743d573f42883265ad63f2c
FilipSekerka <61238315+FilipSekerka@users.noreply.github.com>
2020-11-02 20:02:12 +0100

    vyhodenie duplikavaneho priecinku

--- project_brewster/src/Exceptions/FilesAndFoldersExcetpions/WrongParameterException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions.FilesAndFoldersExcetpions;
-
-public class WrongParameterException extends Exception {
-    public WrongParameterException(String message) {
-        super(message);
-    }
-}
